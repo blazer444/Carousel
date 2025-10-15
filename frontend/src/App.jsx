919ff094c2432a7966c5505849ad10bea734e557
Cauã Carrijo Gomes Silva <kg364141@gmail.com>
2025-10-15 13:45:55 -0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Carousel from "./components/Carousel";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-orange-600 to-yellow-400 flex items-center justify-center">
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
